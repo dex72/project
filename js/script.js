7417cf70edaa084d7bcb66eb88e9590264408819
dex72 <dex72@inbox.ru>
2021-03-12 17:13:00 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,70 @@
+"use strick";
+
+// let number = 5.7;
+
+// // console.log(-4/0);
+// // console.log('string' * 9);
+
+// const persone = `Alex`;
+// // console.log(persone);
+
+// const object = {
+//     name: "John",
+//     age: 25,
+//     isMarried: false,
+// };
+
+// // console.log(object.isMarried);
+
+// let arr = ['plum.png', 'orange.jpg', 6, 'apple.bmp', {}, []];
+// console.log(arr[0]);
+
+// alert('Hello');
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = +prompt("Вам есть 18 лет?", "19");
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('What your name?', '');
+// answers[1] = prompt('What your last name?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// console.log(typeof(answers));
+// console.log(typeof(null));
+
+// const category = 'toys';
+
+// console.log(`https://someurl.com/${category}/5`);
+
+// const user = "Ivan";
+
+// alert(`Привет, ${user}`);
+
+// console.log('arr' + " - object");
+// console.log(4 + +"5");
+
+let incr = 10,
+    decr = 10;
+
+// ++incr;
+// --decr;
+
+console.log(++incr);
+console.log(--decr);
+
+console.log(5%2);
+
+console.log(2*4 === 8);
+
+const isChecker = true,
+      isClose = false;
+
+console.log(isChecker || isClose);
+
+
+
+
