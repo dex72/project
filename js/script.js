2fe6c7bb53cd4379e70094a195889ab10df0b685
dex72 <dex72@inbox.ru>
2021-03-14 01:28:44 +0300

    Условия

--- js/script.js
+++ js/script.js
@@ -65,23 +65,58 @@
 
 // console.log(isChecker || isClose);
 
-const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+// const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
 
-const personalMovieDb = {
-    count: numberOfFilms,
-    movies: {},
-    actors: {},
-    gentres: [],
-    privat: false
+// const personalMovieDb = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     gentres: [],
+//     privat: false
 
-};
+// };
 
-const a = prompt('Один из просмотренных фильмов?', ''),
-      b = prompt('На сколько оцените его?', ''),
-      c = prompt('Один из просмотренных фильмов?', ''),
-      d = prompt('На сколько оцените его?', '');
+// const a = prompt('Один из просмотренных фильмов?', ''),
+//       b = prompt('На сколько оцените его?', ''),
+//       c = prompt('Один из просмотренных фильмов?', ''),
+//       d = prompt('На сколько оцените его?', '');
 
-      personalMovieDb.movies.a = b;
-      personalMovieDb.movies.c = d;
+//       personalMovieDb.movies.a = b;
+//       personalMovieDb.movies.c = d;
 
-      console.log(personalMovieDb);
+//       console.log(personalMovieDb);
+
+// if (4 == 4) {
+//     console.log('ok!');
+// } else {
+//     console.log('Error!');
+// }
+
+// const num = 50;
+
+// if (num < 49) {
+//     console.log('Error');
+// } else if (num > 100) {
+//     console.log('Много');
+// } else {
+//     console.log('Ok');
+// }
+
+// (num === 50) ? console.log('OK!') : console.log('Error');
+
+const num = 1000;
+
+switch (num) {
+    case 49:
+        console.log('Неверно');
+        break;
+    case 100:
+        console.log('Неверно');
+        break;
+    case 50:
+        console.log('В точку');
+        break;
+    default:
+        console.log('Не в этот раз');
+        break;
+}
