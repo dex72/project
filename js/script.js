597ac910dbf54b397518e1f48b18ce7fec6dc03b
dex72 <dex72@inbox.ru>
2021-03-18 17:16:06 +0300

    Cicles

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-"use strick";
+'use strict';
 
 // let number = 5.7;
 
@@ -121,7 +121,7 @@
 //         break;
 // }
 
-let num = 50;
+// let num = 50;
 
 // // while (num < 55) {
 // //     console.log(num);
@@ -129,17 +129,57 @@ let num = 50;
 // // }
 
 // do {
-//     console.log(num);
-//     num++;
-// }
-// while (num <= 55);
+// //     console.log(num);
+// //     num++;
+// // }
+// // while (num <= 55);
 
-for (let i = 1; i < 10; i++) {
-    if (i === 6) {
-        // break;
-        continue;
-    }    
+// for (let i = 1; i < 10; i++) {
+//     if (i === 6) {
+//         // break;
+//         continue;
+//     }    
     
-    console.log(i);
-    
+//     console.log(i);    
+// }
+
+// const numberOfFilms = +prompt('Сколько фильмов вы уже просмотрели?', '');
+
+// const personalMovieDb = {
+//     count: numberOfFilms,
+//     movies: {},
+//     actors: {},
+//     genres: [],
+//     privat: false
+// };
+
+// for (let i = 0; i < 2; i++) {
+//     const a = prompt('Один из просмотренных фильмов?', ''),
+//           b = prompt('На сколько оцените его?', '');
+
+//     if (a != null && b != null && a != '' && b != '' && a.lenght < 50) {
+//         personalMovieDb.movies[a] = b;
+//         console.log('done');
+//     } else {
+//         console.log('error');
+//         i--;
+//     }
+// }
+
+// if (personalMovieDb.count < 10) {
+//     console.log("Просмотрено довольно мало фильмов");
+// } else if (personalMovieDb.count >= 10 && personalMovieDb.count < 30) {
+//     console.log("Вы классический зритель");
+// } else if (personalMovieDb >=30) {
+//     console.log("Вы киноманн!!");
+// } else {
+//     console.log("ERROR!!");
+// }
+
+//     console.log(personalMovieDb);
+
+function showFirstMessage(text) {
+    console.log(text);
 }
+
+showFirstMessage("Hello World!!");
